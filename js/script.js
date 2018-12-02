e1f2b2803752046f65767272fe62a4f7405509ba
Liu <lzmatjp@gmail.com>
2018-12-02 12:16:44 +0900

    updating template

---
+++ js/script.js
@@ -0,0 +1,3 @@
+jQuery(function(){
+    const h1 = <h1>Hello world</h1>;
+});
---
+++ server.js
@@ -0,0 +1,39 @@
+//npm init
+//npm install express --save
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.static('public'));
+
+//------------------------- html -----------------------------
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/html/index.html');
+});
+
+//------------------------- js -----------------------------
+
+app.get('/js/jquery-3.3.1.min.js', function (req, res) {
+  res.sendFile(__dirname + '/js/jquery-3.3.1.min.js');
+});
+
+app.get('/js/react.production.min.js', function (req, res) {
+  res.sendFile(__dirname + '/js/react.production.min.js');
+});
+
+app.get('/js/react-dom.production.min.js', function (req, res) {
+  res.sendFile(__dirname + '/js/react-dom.production.min.js');
+});
+
+app.get('/js/script.js', function (req, res) {
+  res.sendFile(__dirname + '/js/script.js');
+});
+
+//---------------------- log of server started ---------------------
+
+app.listen(PORT, () => {
+    console.log(`React Server is listening on port ${PORT}`);
+  }
+
+);
